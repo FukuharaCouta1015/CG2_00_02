1738d99e7221e7ade8169bff3855f5a5d767f528
FukuharaCouta1015 <k024g0088@g.neec.ac.jp>
2025-04-23 15:55:09 +0900

    00_04

--- main.cpp
+++ main.cpp
@@ -3,6 +3,45 @@
 #include <string>
 #include <format>
 
+std::wstring ConvertString(const std::string& str) {
+    if (str.empty()) {
+        return std::wstring();
+    }
+
+    auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+
+    if (sizeNeeded == 0) {
+        return std::wstring();
+    }
+    std::wstring result(sizeNeeded, 0);
+    MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+    return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+    if (str.empty()) {
+        return std::string();
+    }
+    auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+
+    if (sizeNeeded == 0) {
+        return std::string();
+    }
+
+    std::string result(sizeNeeded, 0);
+
+    WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), &result[0], sizeNeeded, NULL, NULL);
+
+    return result;
+}
+
+
+void Log(const std::string& message)
+{
+    OutputDebugStringA(message.c_str());
+}
+
+
 
 // ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
@@ -70,6 +109,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
     ShowWindow(hwnd, SW_SHOW);
 
+            Log(ConvertString(std::format(L"-------------------------------WSTRING{}\n", L"abc")));
+
     MSG msg {};
     // ウィンドウのxボタンが押されるまでループ
     while (msg.message != WM_QUIT) {
@@ -83,7 +124,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
             // ゲームの更新処理を行う
             // ここにゲームの更新処理を書く
         }
+
     }
+
+        Log(ConvertString(std::format(L"-------------------------------WSTRING{}\n", L"abc")));
 
     return 0;
 }
