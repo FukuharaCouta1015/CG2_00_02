3c669d92ed298f8d982e47c231e94377ec3197ed
FukuharaCouta1015 <k024g0088@g.neec.ac.jp>
2025-05-20 10:07:03 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -122,8 +122,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
     IDXGIAdapter4* uesAdapter = nullptr;
 
-    for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&uesAdapter)) !=
-        DXGI_ERROR_NOT_FOUND; ++i) {
+    for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&uesAdapter)) != DXGI_ERROR_NOT_FOUND; ++i) {
         DXGI_ADAPTER_DESC3 adapterDesc {};
         hr = uesAdapter->GetDesc3(&adapterDesc);
         assert(SUCCEEDED(hr));
@@ -135,28 +134,128 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     }
     assert(uesAdapter != nullptr);
 
-     ID3D12Device* device = nullptr;
+    ID3D12Device* device = nullptr;
 
-     D3D_FEATURE_LEVEL featureLevels[] = {
-         D3D_FEATURE_LEVEL_12_2,
-         D3D_FEATURE_LEVEL_12_1,
-         D3D_FEATURE_LEVEL_12_0,
-     };
+    D3D_FEATURE_LEVEL featureLevels[] = {
+        D3D_FEATURE_LEVEL_12_2,
+        D3D_FEATURE_LEVEL_12_1,
+        D3D_FEATURE_LEVEL_12_0,
+    };
 
-     const char* featureLevelsStrings[] = { "12.2", "12.1", "12.0" };
+    const char* featureLevelsStrings[] = { "12.2", "12.1", "12.0" };
 
-     for (size_t i = 0; i < _countof(featureLevels); ++i) {
-         hr = D3D12CreateDevice( uesAdapter,featureLevels[i],IID_PPV_ARGS(&device));
-         if (SUCCEEDED(hr)) {
-             Log(std::format("FeatureLevel:{}\n", featureLevelsStrings[i]));
-             break;
-         }
-     }
-     assert(device != nullptr);
-     Log("Complete create  D3D12Device!!!\n");
-
+    for (size_t i = 0; i < _countof(featureLevels); ++i) {
+        hr = D3D12CreateDevice(uesAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+        if (SUCCEEDED(hr)) {
+            Log(std::format("FeatureLevel:{}\n", featureLevelsStrings[i]));
+            break;
+        }
+    }
+    assert(device != nullptr);
+    Log("Complete create  D3D12Device!!!\n");
 
     Log(ConvertString(std::format(L"-------------------------------WSTRING{}\n", L"abc")));
+
+    // コマンドキューを生成する
+    ID3D12CommandQueue* commandQueue = nullptr;
+    D3D12_COMMAND_QUEUE_DESC commandQueueDesc {};
+    hr = device->CreateCommandQueue(&commandQueueDesc,
+        IID_PPV_ARGS(&commandQueue));
+
+    assert(SUCCEEDED(hr));
+
+    // コマンドアロケータを生成する
+    ID3D12CommandAllocator* commandAllocator = nullptr;
+    hr = device->CreateCommandAllocator(
+        D3D12_COMMAND_LIST_TYPE_DIRECT,
+        IID_PPV_ARGS(&commandAllocator));
+    // コマンドアロケータの生成がうまくいかなかったので起動出来ない
+    assert(SUCCEEDED(hr));
+
+    // コマンドリストを生成する
+
+    ID3D12GraphicsCommandList* commandList = nullptr;
+    hr = device->CreateCommandList(
+        0,
+        D3D12_COMMAND_LIST_TYPE_DIRECT,
+        commandAllocator,
+        nullptr,
+        IID_PPV_ARGS(&commandList));
+    // コマンドリストの生成がうまくいかなかったので起動出来ない
+    assert(SUCCEEDED(hr));
+
+    // スワップチェーンを生成する
+    IDXGISwapChain4* swapChain = nullptr;
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc {};
+    swapChainDesc.Width = kClinetWidth; // 画面の幅,ウィンドウのクライアント領域を同じものにする
+    swapChainDesc.Height = kClineHeigth; // 画面の高さ,ウィンドウのクライアント領域を同じものにする
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // 画面の形式
+    swapChainDesc.SampleDesc.Count = 1; //マルチサンプルしない
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // 描画のターゲットとして利用する
+    swapChainDesc.BufferCount = 2; // バッファの数
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; //　モニターにうつしたら中身を破棄
+
+    hr = dxgiFactory->CreateSwapChainForHwnd(
+        commandQueue, 
+        hwnd, 
+        &swapChainDesc, 
+        nullptr, 
+        nullptr, 
+        reinterpret_cast<IDXGISwapChain1**>(&swapChain)); // スワップチェーンの取得
+    assert(SUCCEEDED(hr));
+
+    // ディスクリプタヒープの生成
+    ID3D12DescriptorHeap* rtvDescriptorHeap = nullptr;
+    D3D12_DESCRIPTOR_HEAP_DESC rtvDescriptorHeapDesc {};
+    rtvDescriptorHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV; // レンダーターゲットビュー用
+    rtvDescriptorHeapDesc.NumDescriptors = 2; // ダブルバッファように2つ,多くても別にかまわない
+    hr = device->CreateDescriptorHeap(&rtvDescriptorHeapDesc, IID_PPV_ARGS(&rtvDescriptorHeap));
+    assert(SUCCEEDED(hr));
+
+    // SwapChainからResourceを取得する
+    ID3D12Resource* swapChainResources[2] = {nullptr};
+    hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&swapChainResources[0]));
+    assert(SUCCEEDED(hr));
+    hr = swapChain->GetBuffer(1, IID_PPV_ARGS(&swapChainResources[1]));
+    assert(SUCCEEDED(hr));
+
+    //RTVの設定
+    D3D12_RENDER_TARGET_VIEW_DESC rtvDesc {};
+    rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB; // 出力結果をSRGBに変換して書き込む
+    rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D; // 2Dテクスチャ
+    // ディスクリプタの先頭を取得する
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvStartHandle = rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+    //2つ用意
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
+    //1つめを作る
+    rtvHandles[0] = rtvStartHandle;
+    device->CreateRenderTargetView(swapChainResources[0], &rtvDesc, rtvHandles[0]);
+    // 2つめのディスクリプタハンドルを得る
+    rtvHandles[1].ptr = rtvHandles[0].ptr + device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV); 
+    // 2つめを作る
+    device->CreateRenderTargetView(swapChainResources[1], &rtvDesc, rtvHandles[1]);
+
+    // これから書き込むバックバファの禁書目録を取得
+    UINT backBufferindex = swapChain->GetCurrentBackBufferIndex();
+    //　描画先のRTVを設定する
+    commandList->OMSetRenderTargets(1, &rtvHandles[backBufferindex], false, nullptr);
+    // 指定した色で画面全体をクリアする
+    float clearColor[] = { 0.1f, 0.25f, 0.5f, 1.0f };
+    commandList->ClearRenderTargetView(rtvHandles[backBufferindex], clearColor, 0, nullptr);
+    // コマンドリストの内容を確定させる。全てのコマンドを積んでからCloseすること
+    hr = commandList->Close();
+    assert(SUCCEEDED(hr));
+    // GPUにコマンドリストを実行させる
+    ID3D12CommandList* commandLists[] = { commandList };
+    commandQueue->ExecuteCommandLists(1, commandLists);
+    // GPUとosに画面の交換を行うように通知する
+    swapChain->Present(1, 0);
+    // 次のフレーム用のコマンドリストを準備
+    hr = commandAllocator->Reset();
+    assert(SUCCEEDED(hr));
+    hr = commandList->Reset(commandAllocator, nullptr);
+    assert(SUCCEEDED(hr));
+
 
     MSG msg {};
     // ウィンドウのxボタンが押されるまでループ
